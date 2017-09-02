e5ac24dc7d2ad0ab9fb737a7e524f7185641899f
Ted Kim <tkk21@case.edu>
2017-09-02 13:49:15 -0400

    add dot file

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,44 @@
+echo "Welcome to PowerShell: $env:Username"
+
+# To edit the Powershell Profile
+Function Edit-Profile
+{
+    vim $profile
+}
+
+# To edit Vim settings
+Function Edit-Vimrc
+{
+    vim $HOME\_vimrc
+}
+
+Function Elevate-Powershell
+{
+    Start-Process powershell -verb runas
+}
+
+#Set-ExecutionPolicy RemoteSigned
+
+# custom variables
+$Torrent = "H:/Torrent"
+$MEGA = "H:/MEGA/download"
+$Hosts = "C:\Windows\System32\drivers\etc\hosts"
+
+# custom Aliases
+set-alias open ii
+set-alias grep select-string
+
+# custom application Aliases
+set-alias vim "C:/Program Files (x86)/Vim/vim80/./vim.exe"
+set-alias chrome "C:/Program Files (x86)/Google/Chrome/Application/./chrome.exe"
+set-alias su Elevate-Powershell 
+set-alias ll ls
+set-alias l ls
+set-alias scp pscp.exe
+set-alias ssh plink.exe
+
+# Bash-like tab completion
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+# Import my volume library
+Import-Module -Name ~/scripts/Set-SpeakerVolume.psm1 -WarningAction SilentlyContinue
